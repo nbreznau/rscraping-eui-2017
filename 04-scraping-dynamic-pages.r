ba6efaf3291e7f2a02ff711ebb25b02cf9b5f542
Simon Munzert <simon.munzert@gmail.com>
2017-05-19 09:45:37 +0200

    Updated Selenium materials

--- 04-scraping-dynamic-pages.r
+++ 04-scraping-dynamic-pages.r
@@ -1,6 +1,6 @@
 ### -----------------------------
 ### simon munzert
-### scraping with selenium
+### scraping dynamic webpages
 ### -----------------------------
 
 ## peparations -------------------
@@ -9,89 +9,41 @@ source("00-course-setup.r")
 wd <- getwd()
 
 
-system(paste0("cd ", getwd()))
-system("java -jar selenium-server-standalone-3.0.0-beta4.jar")
-system("java -jar selenium-server-standalone-2.53.1.jar")
-
-
-
-## web inspection tools ---------
-
-# Chrome, Firefox:
-# right-click on element, then "Inspect Element"
-
-# Safari:
-# Settings --> Advanced --> Show Develop menu in menu bar
-# Web inspector tools visible in menu bar - the console is most useful
-
-# Edge:
-# go to google.com/chrome/ or mozilla.org/en-US/firefox/new/ and download Chrome/Firefox
-# next: see above
-
-
-## let's go to the buzzfeed.com page again and see dynamic webpages in action
-browseURL("https://www.buzzfeed.com/?country=us")
-
 
 ## setup R + RSelenium -------------------------
 
-# install current version of Java
-browseURL("https://www.java.com/en/download/")
-  
+# install current version of Java SE Runtime Environment
+browseURL("http://www.oracle.com/technetwork/java/javase/downloads/jre8-downloads-2133155.html")
+
 # install current version of Firefox browser
 browseURL("https://www.mozilla.org/en-US/firefox/new/") # might not always work, probably go with version 48 instead?
 
 # install dev version of RSelenium
 devtools::install_github("ropensci/RSelenium")
 
-# note: on a Windows or Linux machine, using Docker might be useful; see
-vignette("RSelenium-docker", package = "RSelenium") 
+# set up connection via RSelenium package
+# documentation: http://cran.r-project.org/web/packages/RSelenium/RSelenium.pdf
+
+# check currently installed version of Java
+system("java -version")
+#system("java -jar selenium-server-standalone-3.4.0.jar")
 
 
 ## example --------------------------
 
+# initiate Selenium driver
+rD <- rsDriver()
+remDr <- rD[["client"]]
+
+# start browser, navigate to page
 url <- "http://www.iea.org/policiesandmeasures/renewableenergy/"
-browseURL(url)
-content <- read_html(url)
-
-# set up connection via RSelenium package
-# documentation: http://cran.r-project.org/web/packages/RSelenium/RSelenium.pdf
-
-# retrieve Selenium Server binary if necessary
-checkForServer() ## deprecated; but gives advice on how to source/start a server
-
-# start server
-startServer()  ## deprecated
-
-# my solution on a Mac: 
-  # install latest Java Development Kit (JDK)
-  # install selenium standalone server, current beta
-  # open Terminal, navigate to folder where standalone server file is deposited
-  # check whether processes run on port 4444 with lsof -i:4444; potentially kill them using PID
-  # run the following line in Terminal: java -jar selenium-server-standalone-3.0.0-beta4.jar
-  # open Firefox developer tools before sending stuff!
-
-
-
-
-# assume gecko driver is not in our path (assume windows and we downloaded to docs folder)
-# if the driver is in your PATH the javaargs call is not needed
-
-# connect to server
-remDr <- remoteDriver(remoteServerAddr = "localhost", port = 4444, browserName = "firefox") 
-
-# open connection; Firefox window should pop up
-remDr$open() 
-
-# navigate to data request page
-remDr$navigate(url) 
-
-# open web inspector tools (on a Mac with my setup, the following code did not work without it)
+remDr$navigate(url)
 
 # open regions menu
 css <- 'div.form-container:nth-child(2) > ul:nth-child(2) > li:nth-child(1) > span:nth-child(1)'
 regionsElem <- remDr$findElement(using = 'css', value = css)
 openRegions <- regionsElem$clickElement() # click on button
+
 
 
 # selection "European Union"
@@ -144,6 +96,4 @@ head(tab)
 # 3. download the data, import it into R and try to plot the stores on a map!
 
 browseURL("http://www.starbucks.com/store-locator/search/location/chicago")
-
-
 

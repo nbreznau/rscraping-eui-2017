0aabe8ef7d07500239fc7256430305df18545b4c
Nate Breznau <breznau.nate@gmail.com>
2018-09-05 09:26:47 +0200

    Update 00-course-setup.r
    For those who can't install Rselenium for their (newer) version of R

--- 00-course-setup.r
+++ 00-course-setup.r
@@ -13,3 +13,9 @@ if (length(p_to_install) > 0) {
 }
 
 lapply(p_needed, require, character.only = TRUE)
+
+# in case Rselenium is not available for your R version (mine was 3.5.1), this is a workaround
+library(devtools)
+install_version("binman", version = "0.1.0", repos = "https://cran.uni-muenster.de/")
+install_version("wdman", version = "0.2.2", repos = "https://cran.uni-muenster.de/")
+install_version("RSelenium", version = "1.7.1", repos = "https://cran.uni-muenster.de/")
